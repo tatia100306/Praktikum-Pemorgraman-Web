5f9984a3cf4f201b79374bbd7bee7e1a76a12a24
tatia100306 <tiadwers@gmail.com>
2025-10-15 16:24:22 +0800

    Add files via upload

---
+++ praktikum6/test3.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Halaman PHP pertamaku</title>
+    </head>
+    <body>
+        <h1>Script PHP dalam (X)HTML</h1>
+        <?php
+            echo "Hello World!";
+            echo "Hello World!";
+            echo "Hello World!";
+            echo "Hello World!";
+        ?>
+    </body>
+</html>
